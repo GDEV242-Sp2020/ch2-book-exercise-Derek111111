4d4c94d697554242b719ce6a814424976e3b0f05
Derek111111 <44411373+Derek111111@users.noreply.github.com>
2020-02-03 12:45:04 -0500

    Forgot to commit steps, did steps 83-87

--- Book.java
+++ Book.java
@@ -11,16 +11,65 @@ class Book
      // The fields.
     private String author;
     private String title;
+    private String genre;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     // Add the methods here ...
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public String getGenre()
+    {
+     return genre;   
+    }
+    
+    public String getPages()
+    {
+     return pages;   
+    }
+    
+    public void printAuthor()
+    {
+      System.out.println("Author: " + author);   
+    }
+    
+    public void printTitle()
+    {
+     System.out.println("Title: " + title);   
+    }
+    
+    public void printGenre()
+    {
+     System.out.println("Genre: " + genre);    
+    }
+    
+    public void printPages()
+    {
+     System.out.println("Pages: " + pages);   
+    }
+    
+    public void printDetails()
+    {
+     System.out.println("Book Author: " + author);  
+     System.out.println("Book Title: " + title);  
+     System.out.println("Book Pages: " + pages);   
+    }
 }
