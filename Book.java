ab90d8c9bfe5027909806c08488a139f949b5341
Derek Ammerlaan <18dammerlaan@gmail.com>
2020-02-09 21:28:08 -0500

    All excersizes finished

--- Book.java
+++ Book.java
@@ -1,75 +1,176 @@
-/**
- * A class that maintains information on a book.
- * This might form part of a larger application such
- * as a library system, for instance.
- *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
- */
+            /**
+             * author: Derek Ammerlaan
+             * Book Class
+             */
 class Book
 {
-     // The fields.
+                 // The fields.
     private String author;
     private String title;
     private String genre;
     private int pages;
-
+    private String refNumber;
+    private int borrowed;
+    private boolean courseText;
+            
     /**
-     * Set the author and title fields when this object
-     * is constructed.
+     * Constructor, will set author, title, pages, and
+     * coursetext fields to their respective parameters.
      */
-    public Book(String bookAuthor, String bookTitle, int bookPages)
+    public Book(String bookAuthor, String bookTitle, 
+                int bookPages, boolean isCourseText)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        refNumber = "";
+        borrowed = 0;
+        courseText = isCourseText;
     }
 
-    // Add the methods here ...
+    /**
+     * Return the value of the author field
+     * @return: author of the book
+     */
     public String getAuthor()
     {
         return author;
     }
     
+    /**
+     * Return the title field of the book
+     * @return: title of book
+     */
     public String getTitle()
     {
         return title;
     }
     
+    /**
+     * Returns the genre field of the book
+     * @return: genre of book
+     */
     public String getGenre()
     {
-     return genre;   
+        return genre;   
     }
     
-    public String getPages()
+    /**
+     * Returns pages field of book
+     * @return: number of pages book has
+     */
+    public int getPages()
     {
-     return pages;   
+        return pages;   
     }
     
+    /**
+     *Returns the refnumber field of the book
+     *@return: refnumber of book
+     */
+    public String getRefNumber()
+    {
+        return refNumber;   
+    }
+    
+    /**
+     * Return borrowed field of book
+     * @return: how many times book was borrowed
+     */
+    public int getBorrowed()
+    {
+        return borrowed;
+    }
+    
+    /**
+     * Returns coursetext field of book
+     * @return: if book is used as a textbook
+     */
+    public boolean isCourseText()
+    {
+        return courseText;
+    }
+    
+    /**
+     * Display the author field of the book object
+     * in the console
+     */
     public void printAuthor()
     {
-      System.out.println("Author: " + author);   
+        System.out.println("Author: " + author);   
     }
     
+    /**
+     * Display the title field of the book object
+     * in the console
+     */
     public void printTitle()
     {
-     System.out.println("Title: " + title);   
+        System.out.println("Title: " + title);   
     }
     
+    /**
+     * Display the genre field of the book object
+     * in the console
+     */
     public void printGenre()
     {
-     System.out.println("Genre: " + genre);    
+        System.out.println("Genre: " + genre);    
     }
     
+    /**
+     * Takes in parameter ref and sets the
+     * refNumber field to ref if ref has
+     * at least 3 characters
+     * @param: String ref to be assigned to 
+     * refNumber field
+     */
+    public void setRefNumber(String ref)
+    {
+        if(ref.length() > 3)
+        {
+            refNumber = ref; 
+        }else
+        {
+            System.out.println("Error: Parameter was not at " +
+                                 "least 3 characters long");
+        }
+    }
+    
+    /**
+     * Increased the borrowed field by 1 
+     */
+    public void incrementBorrowed()
+    {
+        borrowed++;
+    }
+    
+    /**
+     * Displays the pages field of the book object
+     * in the console
+     */
     public void printPages()
     {
-     System.out.println("Pages: " + pages);   
+        System.out.println("Pages: " + pages);   
     }
     
+    /**
+     * Display a formatted text of author, title
+     * pages, borrowed fields of the book, as well
+     * as refNumber if it has been assigned a value
+     */
     public void printDetails()
     {
-     System.out.println("Book Author: " + author);  
-     System.out.println("Book Title: " + title);  
-     System.out.println("Book Pages: " + pages);   
+         String printString = "Author: " + author + ", Title: " + 
+                            title + ", Pages: " + pages;
+         if(refNumber.length() > 0)
+         {
+             printString = printString + ", RefNumber: " + refNumber;
+         }else
+         {
+             printString = printString + "ZZZ";
+         }
+         System.out.println(printString + ", This book has been borrowed "+
+                                            borrowed + " times");   
     }
 }
